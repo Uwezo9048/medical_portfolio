a533a737b6946849927b86a596a66f41e7636b35
unknown <Uwezo9048>
2026-01-15 04:47:00 +0300

    Update file

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,18 @@
 import sys
 import os
 
-# Add the project directory to the path
-path = os.path.dirname(os.path.abspath(__file__))
+# Get your username automatically
+username = os.path.basename(os.path.expanduser('~'))
+path = f'/home/{username}/medical_portfolio'
+
 if path not in sys.path:
     sys.path.insert(0, path)
+
+# Change to your project directory
+os.chdir(path)
 
 # Import the Flask app
 from app import app
 
-# This is needed for PythonAnywhere
+# PythonAnywhere requires the variable to be called 'application'
 application = app
